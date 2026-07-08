d9401ab25de394737c9543fe4ff724340695cc87
Kerr Beeldens <kerrbeeldens@outlook.com>
2026-07-08 23:38:31 +0200

    Finish week 1 exercise B3

---
+++ week_1/opgave_b3_coding_io/main.c
@@ -0,0 +1,26 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define BIT(x) (1 << (x))
+
+int main(void)
+{
+	DDRD |=  BIT(7); // Make D7 output
+	
+	while(1)
+	{
+		
+		if(PIND & BIT(0)) 
+		{
+			PORTD ^= BIT(7);
+		} 
+		else
+		{
+			PORTD &= ~BIT(7);
+		}
+		
+		_delay_ms(500);
+	}
+}
